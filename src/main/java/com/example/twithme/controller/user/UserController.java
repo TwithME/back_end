5cf9236a3090aad7203454e78ab1a76809c9b658
YuJin Kwon <tokyj515@naver.com>
2023-11-24 20:17:12 +0900

    feat: 카카오 소셜 로그인 서버용 로직 추가

--- src/main/java/com/example/twithme/controller/user/UserController.java
+++ src/main/java/com/example/twithme/controller/user/UserController.java
@@ -6,6 +6,7 @@ import com.example.twithme.common.model.ApiResponse;
 import com.example.twithme.model.dto.user.UserReq;
 import com.example.twithme.model.dto.user.UserRes;
 import com.example.twithme.service.user.UserService;
+import com.google.gson.JsonElement;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,9 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Base64;
 import java.util.Map;
 
@@ -22,15 +26,26 @@ import java.util.Map;
 @Api(tags={"01.User"})
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(path = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 
     private final UserService userService;
 
 
-    @ApiOperation(value = "카카오 회원가입", notes = "카카오 회원가입 후 추가 정보를 등록하는 API 입니다.\n" +
-            "/login/kakao을 통해 받은 accessToken을 입력해야만 해당 API 호출이 가능합니다.")
-    @PostMapping("/signup/kakao")
+    @ApiOperation(value = "서버용(프론트 사용x)",
+            notes = "")
+    @GetMapping("/oauth/kakao")
+    public ApiResponse<UserReq.KakaoLogInDto> getAccessTokenKakao(@RequestParam String code) {
+        String accessTokenFromSocial = userService.getKakaoAccessToken(code);
+        UserReq.KakaoLogInDto kakaoLogInDto = userService.createAndLoginKakaoUser(accessTokenFromSocial);
+
+        return new ApiResponse<>(kakaoLogInDto);
+    }
+
+
+    @ApiOperation(value = "카카오 추가 회원가입", notes = "카카오 회원가입 후 추가 정보를 등록하는 API 입니다.\n" +
+            "/user/login/kakao을 통해 받은 accessToken을 입력해야만 해당 API 호출이 가능합니다.")
+    @PostMapping("/user/signup/kakao")
     public ApiResponse<String> kakaoSignUp(@RequestBody UserReq.KakaoSignUpDto kakaoSignUpDto,
                                            HttpServletRequest httpServletRequest) {
         Long userId = userService.getUserId(httpServletRequest);
@@ -40,7 +55,7 @@ public class UserController {
 
 
     @ApiOperation(value = "카카오 로그인", notes = "카카오 계정으로 로그인을 합니다.")
-    @PostMapping("/login/kakao")
+    @PostMapping("/user/login/kakao")
     public ApiResponse<UserRes.UserInfoWithToken> doKakaoLogIn(@RequestBody UserReq.KakaoLogInDto kakaoLogInDto) {
         Base64.Decoder decoder = Base64.getUrlDecoder();
         final String payload = new String(decoder.decode(kakaoLogInDto.getSnsId().split("\\.")[1]));
--- src/main/java/com/example/twithme/service/user/UserService.java
+++ src/main/java/com/example/twithme/service/user/UserService.java
@@ -14,6 +14,8 @@ import com.example.twithme.repository.user.MbtiRepository;
 import com.example.twithme.repository.user.UserHashtagRepository;
 import com.example.twithme.repository.user.UserRepository;
 import com.example.twithme.config.security.JwtTokenProvider;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.*;
 
@@ -347,5 +353,127 @@ public class UserService {
         return optionalUser.get().getId();
 
     }
+
+
+
+    public String getKakaoAccessToken(String code) {
+        String access_Token="";
+        String refresh_Token ="";
+        String reqURL = "https://kauth.kakao.com/oauth/token";
+
+        try{
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            //POST 요청을 위해 기본값이 false인 setDoOutput을 true로
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            //POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            StringBuilder sb = new StringBuilder();
+            sb.append("grant_type=authorization_code");
+            sb.append("&client_id=" + "db83444fceb2cd72e91f38e02179e061");
+            sb.append("&redirect_uri=" + "http://localhost:8081/oauth/kakao");
+            sb.append("&code=" + code);
+            bw.write(sb.toString());
+            bw.flush();
+
+
+            System.out.println("======authService 요청 결과 시작======");
+            //결과 코드가 200이라면 성공
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
+            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body : " + result);
+
+            //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            access_Token = element.getAsJsonObject().get("access_token").getAsString();
+            refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
+
+            System.out.println("access_token for kakao: " + access_Token);
+            System.out.println("refresh_token for kakao: " + refresh_Token);
+            System.out.println("======authService 요청 결과 끝=======");
+
+            br.close();
+            bw.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return access_Token;
+    }
+
+    @Transactional()
+    public UserReq.KakaoLogInDto createAndLoginKakaoUser(String accessTokenFromSocial)  {
+
+        String reqURL = "https://kapi.kakao.com/v2/user/me";
+
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Authorization", "Bearer " + accessTokenFromSocial); //전송할 header 작성, access_token전송
+
+            //결과 코드가 200이라면 성공
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
+
+            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body : " + result);
+
+
+            //카카오에서 받은 정보 파싱
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            String kakaoId = element.getAsJsonObject().get("id").getAsString(); //카카오의 인덱스
+
+
+            //카카오에 저장된 이름
+            String name = element.getAsJsonObject().get("properties").getAsJsonObject().get("nickname").getAsString();
+            JsonElement jsonElement = element.getAsJsonObject().get("properties").getAsJsonObject().get("profile_image");
+            String profileUrl = "";
+            if (jsonElement != null) {
+                profileUrl = jsonElement.getAsString();
+            }
+
+            UserReq.KakaoLogInDto kakaoLogInDto = UserReq.KakaoLogInDto.builder()
+                    .snsId(kakaoId)
+                    .snsToken(accessTokenFromSocial)
+                    .profileImage(profileUrl)
+                    .name(name)
+                    .build();
+
+            return kakaoLogInDto;
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
 
 }
